19f770bd2444d37d3008fa2847bec15f346c3ecb
Michael Nilson <Michael.Nilson@potashcorp.com>
2018-02-28 15:43:27 -0600

    EOD Feb 28

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-console.log('Starting notes app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
-console.log(user.username);
+var filteredArray = _.uniq(['pth2mn', 1, 'pth2mn', 2, 3, 4]);
+console.log(filteredArray);
 
-fs.appendFile('greetings.txt', `hello ${user.username}!`, function(err){
-  if(err){
-    console.log('unable to write file: ' + err);
-  }
-});
+// var res = notes.addNote();
+// console.log(res);
+// console.log(notes.add(9, -2));
+
+// var user = os.userInfo();
+// console.log(user.username);
+//
+// fs.appendFile('greetings.txt', `hello ${user.username}! You are ${notes.age}.`, function(err){
+//   if(err){
+//     console.log('unable to write file: ' + err);
+//   }
+// });
