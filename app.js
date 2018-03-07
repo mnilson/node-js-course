8d91f41b13ff1759414447575e56ee704cd39437
Michael Nilson <Michael.Nilson@potashcorp.com>
2018-03-07 14:12:15 -0600

    Completed notes application

--- app.js
+++ app.js
@@ -5,8 +5,26 @@ const yargs = require('yargs');
 const notes = require('./notes.js');
 
 const command = process.argv[2];
-
-const [argv] = yargs.argv;
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+};
+const bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+};
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', { title: titleOptions })
+    .command('remove', 'Remove a note', { title: titleOptions })
+    .help()
+    .argv;
 
 if (command === 'add') {
     const note = notes.addNote(argv.title, argv.body);
