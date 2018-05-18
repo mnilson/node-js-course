8972e7105dad5d0b40053d0f1c9ad6da8b86ba7b
Michael Nilson <Michael.Nilson@Yardi.com>
2018-05-18 15:39:49 -0600

    Adding usage of Expect library

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,15 +1,25 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var actual = utils.add(1,2);
-  if(actual !== 3){
-    throw new Error(`Expected 3, but got ${actual}.`);
-  }
+  expect(actual).toBe(3).toBeA('number');
 });
 
 it('should square a number', () => {
   var actual = utils.square(2);
-  if(actual !== 4){
-    throw new Error(`Expected 4, but got ${actual}.`);
-  }
+  expect(actual).toBe(4).toBeA('number');
+});
+
+it('should verify first and last names are set', ()=>{
+  var user = {
+    age: '36',
+    location: 'Saskatoon'
+  };
+
+  var actual = utils.setName(user, 'Michael Nilson');
+
+  expect(actual.firstName).toEqual('Michael').toBeA('string');
+  expect(actual.lastName).toEqual('Nilson').toBeA('string');
+  expect(actual).toBeA('object');
 });
